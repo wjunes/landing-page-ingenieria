281adeed7bdd63bd66131defe8b876398ee55208
Willans Junes <willans@tuyaymia.com>
2023-04-27 13:53:43 -0300

    Add files via upload

--- assets/js/dark-ligth.js
+++ assets/js/dark-ligth.js
@@ -1,14 +1,14 @@
 
 
 const toggleBtn = document.querySelector('#mode-selector');
-const container = document.querySelector('body');
+const body = document.querySelector('body');
 const main = document.querySelector('.main');
-const btnCon = document.querySelector('.btn-con');
-const footer = document.querySelector('footer');
+const btnCon = document.querySelector('#mode-selector');
+
 
   toggleBtn.addEventListener('click', function() {
-  container.classList.toggle('dark-mode');
+  body.classList.toggle('dark-mode');
   main.classList.toggle('dark-mode');
   toggleBtn.classList.toggle('dark-mode');
-  footer.classList.toogle('dark-mode');
+ 
 });
